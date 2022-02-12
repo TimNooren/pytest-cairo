cd88834d287b215634922de7c10a9ee33bd29117
TimNooren <timnooren@gmail.com>
2022-02-12 19:30:32 +0100

    Remove Python 3.10 from testing
    Until https://github.com/starkware-libs/cairo-lang/issues/27 is
    fixed.

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,13 +1,4 @@
-import sys
-
-import pytest
 from _pytest.pytester import Pytester
-
-pytestmark = pytest.mark.xfail(
-    sys.version_info >= (3, 10),
-    reason='Issue in cairo-lang. '
-    'See https://github.com/starkware-libs/cairo-lang/issues/27',
-)
 
 
 def test_plugin(pytester: Pytester) -> None:
