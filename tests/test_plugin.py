94419d3c7e61cc185cb2075837c9385c6bcf88db
TimNooren <timnooren@gmail.com>
2022-04-16 08:57:39 +0200

    Register plugin in tests
    To avoid having to install the plugin manually before being able to
    run some tests.

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -9,6 +9,12 @@ def test_plugin(pytester: Pytester) -> None:
 disable_contract_hash_computation=true
 """)
 
+    # REVIEW: Not sure if this is a great idea, but it avoids having to
+    # `pip install .` to test the plugin.
+    pytester.makeconftest("""
+pytest_plugins = ("pytest_cairo.plugin",)
+""")
+
     # Note: `copy_example` requires pytester_example_dir to be set in the
     # pytest config.
     pytester.copy_example(str(RESOURCES_DIR))
